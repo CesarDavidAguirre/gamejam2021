2dee6ddde29cd46f73ae1d474990c31dc5a627e3
Rodri <ernestgen1@gmail.com>
2021-01-30 20:31:47 -0300

    simon persigue

---
+++ Assets/Scenes/RodriScripts/Follow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    Vector2 Enemypos;
+    public GameObject PlayerM;
+    bool followP;
+    public int speed;
+
+    void Update()
+    {
+        if (followP)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Enemypos, speed * Time.deltaTime);
+
+        }
+
+        if (Vector2.Distance(transform.position, Enemypos) > 12f)
+        {
+            followP = false;
+        }
+
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+        {
+            if (collision.tag.Equals("Player"))
+            {
+                Enemypos = PlayerM.transform.position;
+                followP = true;
+            }
+        }
+}
