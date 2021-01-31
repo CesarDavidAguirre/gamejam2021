c501412d40f246525242e22535b23b53a9575124
Rodri <ernestgen1@gmail.com>
2021-01-31 12:19:09 -0300

    simon persiguir2

---
+++ Assets/Scenes/NivelRodri/RodriScripts/Payaso1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Payaso1 : MonoBehaviour
+{
+    public Image barVidaPayaso;
+    float vida = 2000;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("disparoPJ"))
+        {
+            Destroy(collision.gameObject);
+            vida = vida - 20;
+            if (vida == 0) SceneManager.LoadScene("menu");
+            barVidaPayaso.fillAmount = vida / 2000;
+        }
+    }
+}
