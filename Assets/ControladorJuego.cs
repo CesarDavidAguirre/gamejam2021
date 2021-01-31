1ceba2246a60d63323c3cbced3829b1f3d4ab2ec
CarlosMerca <carlos.94.012@gmail.com>
2021-01-30 21:53:15 -0300

    NivelVoladorymas

---
+++ Assets/ControladorJuego.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorJuego : MonoBehaviour
+{
+    public int VidaPersonaje = 3;
+    public int VidaBoss = 1000;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
